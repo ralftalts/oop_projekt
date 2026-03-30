e46a9ac1f1c560b392afef324a5806d43b65298a
iristikerpuu <tikerpuu.iris@gmail.com>
2026-03-30 15:58:25 +0300

    Update Mängija.java

--- Mängija.java
+++ Mängija.java
@@ -1,1 +1,19 @@
-iris
+public class Mängija {
+
+    private String nimi;
+    private int punktid;
+
+    public Mängija(String nimi) {
+        this.nimi = nimi;
+        this.punktid = 0;
+    }
+    public void lisaPunkt() {  // lisab mängijale punkti
+        this.punktid++;
+    }
+    public int getPunktid() {  // kui pärast vaja punkte näha
+        return punktid;
+    }
+    public String getNimi() {
+        return nimi;
+    }
+}
