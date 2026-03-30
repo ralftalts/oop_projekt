d124025204d83595169ac577dd3f2523dcdcbf7b
ralftalts <taltsralf@gmail.com>
2026-03-30 16:20:11 +0300

    Update and rename Küsimus.java to Kysimus.java

---
+++ Kysimus.java
@@ -0,0 +1,59 @@
+
+public class Kysimus {
+
+    private String   tekst;        // Küsimuse tekst
+    private String[] vastused;     // 4 vastusevariant
+    private int      oigeIndeks;   // Õige vastuse indeks (1-4)
+
+    public Kysimus(String tekst, String[] vastused, int oigeIndeks) {
+        this.tekst      = tekst;
+        this.vastused   = vastused;
+        this.oigeIndeks = oigeIndeks;
+    }
+
+
+    public String getTekst() {
+        return tekst;
+    }
+    public String[] getVastused() {
+        return vastused;
+    }
+    public int getOigeIndeks() {
+        return oigeIndeks;
+    }
+    public String getKysimus() {
+        return tekst;
+    }
+    public int getVastus() {
+        return oigeIndeks;
+    }
+    public String getVariant1() { return vastused[0]; }
+    public String getVariant2() { return vastused[1]; }
+    public String getVariant3() { return vastused[2]; }
+    public String getVariant4() { return vastused[3]; }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+    public void setVastused(String[] vastused) {
+        this.vastused = vastused;
+    }
+    public void setOigeIndeks(int oigeIndeks) {
+        this.oigeIndeks = oigeIndeks;
+    }
+    public boolean onOige(int vastus) {
+        return vastus == oigeIndeks;
+    }
+
+    public String getOigeVastus() {
+        return vastused[oigeIndeks - 1];
+    }
+
+    public void kuva() {
+        System.out.println("\n  " + tekst);
+        System.out.println();
+        for (int i = 0; i < vastused.length; i++) {
+            System.out.println("  " + (i + 1) + ") " + vastused[i]);
+        }
+    }
+}
