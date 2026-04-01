987beac30a6da22265af8da91f656b88364d8b61
iristikerpuu <tikerpuu.iris@gmail.com>
2026-04-02 01:26:53 +0300

    Update Mangija.java

--- Mangija.java
+++ Mangija.java
@@ -1,16 +1,16 @@
 public class Mangija {
 
-    private String nimi;
-    private int punktid;
+    private String nimi; // Mängija nimi
+    private int punktid; // Mängija punktid
 
     public Mangija(String nimi) {
         this.nimi = nimi;
-        this.punktid = 0;
+        this.punktid = 0; // Punktide algväärtus on 0, millele õigete vastuste korral hakatakse punkte lisama
     }
-    public void lisaPunkt() {  // lisab mängijale punkti
+    public void lisaPunkt() {  // Lisab mängijale punkti õige vastuse korral
         this.punktid++;
     }
-    public int getPunktid() {  // kui pärast vaja punkte näha
+    public int getPunktid() {  // Kui hiljem vaja punktiseisu kuvada
         return punktid;
     }
     public String getNimi() {
