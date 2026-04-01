707d3a7f01c4545620d417180ec75c1433bb9d66
iristikerpuu <tikerpuu.iris@gmail.com>
2026-04-02 01:25:25 +0300

    Update Peaklass.java

--- Peaklass.java
+++ Peaklass.java
@@ -7,33 +7,33 @@ public class Peaklass {
         System.out.println("Vasta küsimustele, sisestades õige vastusevariandi numbri.");
         System.out.println("Iga õige vastuse korral lisatakse sulle 1 punkt.\n");
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Kasutaja sisendi lugemiseks
         
         System.out.print("Sisesta oma nimi: ");
-        String nimi = scanner.nextLine();
+        String nimi = scanner.nextLine(); // Scanner loeb kasutaja sisestatud nime
         Mangija mängija = new Mangija(nimi);
         
         Mang mang = new Mang("kysimused.txt");
         
         int kysimusteKogus = mang.getKysimusteArv();
-        for (int i = 0; i < kysimusteKogus; i++) {
+        for (int i = 0; i < kysimusteKogus; i++) { // Tsükkel küsimuste küsimiseks ja vastuste kontrollimiseks
 
             Kysimus kysimus = mang.juhuslikKysimus();
             
             kysimus.kuva();
             
             System.out.print("\nSisesta õige vastuse number (1-4): ");
-            int vastus = scanner.nextInt();
+            int vastus = scanner.nextInt(); // Loeb kasutaja sisestatud vastusevariandi numbri
             scanner.nextLine(); // puhasta buffer
             
-            if (mang.kontrolliVastus(kysimus, vastus)) {
+            if (mang.kontrolliVastus(kysimus, vastus)) {  // Kui meetod kontrolliVastus tagastab "true", lisatakse mängijale punkt
                 mängija.lisaPunkt();
                 System.out.println("Oige vastus! Teenisid uhe punkti.");
             } else {
-                System.out.println("Vale vastus! Oige vastus oli: " + kysimus.getOigeVastus());
+                System.out.println("Vale vastus! Oige vastus oli: " + kysimus.getOigeVastus()); // Väljastatakse ka õige vastus
             }
 
-            System.out.println("Punktid: " + mängija.getPunktid() + "/" + (i + 1));
+            System.out.println("Punktid: " + mängija.getPunktid() + "/" + (i + 1)); // Väljastab igal korral ka hetkese punktiseisu
         }
         
         System.out.println("\nMang sai labi!");
