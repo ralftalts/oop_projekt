294bd3b3b74ae7ab56f8e2d82bc4876e0c30d861
ralftalts <taltsralf@gmail.com>
2026-05-13 15:00:48 +0300

    Update Mangija.java

--- Mangija.java
+++ Mangija.java
@@ -1,17 +1,20 @@
 public class Mangija {
 
-    private String nimi; // Mängija nimi
-    private int punktid; // Mängija punktid
+    private String nimi; // Mangija nimi
+    private int punktid; // Mangija punktid
 
     public Mangija(String nimi) {
         this.nimi = nimi;
         this.punktid = 0; // Punktide algväärtus on 0, millele õigete vastuste korral hakatakse punkte lisama
     }
-    public void lisaPunkt() {  // Lisab mängijale punkti õige vastuse korral
+    public void lisaPunkt() {  // Lisab mangijale punkti õige vastuse korral
         this.punktid++;
     }
     public int getPunktid() {  // Kui hiljem vaja punktiseisu kuvada
         return punktid;
+    }
+    public void eemaldaPunkt() {  // Eemaldab punkti käigu tagasivõtmisel
+        if (this.punktid > 0) this.punktid--;
     }
     public String getNimi() {
         return nimi;
